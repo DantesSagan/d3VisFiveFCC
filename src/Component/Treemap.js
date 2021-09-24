5913bff000db690f25ba117f7ae3d6ef64353f60
lexsander ikhaylov <mihaleksval@gmail.com>
2021-09-24 22:22:49 +0500

    separated number and data-value is arrived!

--- src/Component/Treemap.js
+++ src/Component/Treemap.js
@@ -2,6 +2,7 @@
 import { useState, useEffect, useRef } from 'react';
 
 import { treemap } from 'd3-hierarchy';
+import { pointer } from 'd3-selection';
 import * as d3 from 'd3';
 
 import React from 'react';
@@ -10,13 +11,14 @@ export default function Treemap({ width, height }) {
   const [kickStarterPledges] = useState(
     'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json'
   );
-  const [movieSales] = useState(
-    'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
-  );
-  const [videoGameSales] = useState(
-    'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json'
-  );
+  // const [movieSales] = useState(
+  //   'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
+  // );
+  // const [videoGameSales] = useState(
+  //   'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json'
+  // );
   const svgRef = useRef(null);
+  const legendRef = useRef(null);
   let dataKickStarter;
 
   const drawTreeMap = () => {
@@ -27,6 +29,7 @@ export default function Treemap({ width, height }) {
 
     const svg = d3.select(svgRef.current);
     svg.attr('width', width).attr('height', height);
+    svg.selectAll('g').remove();
 
     const treeMapRoot = treemap().size([width, height]).padding(1)(root);
 
@@ -35,9 +38,20 @@ export default function Treemap({ width, height }) {
       .data(treeMapRoot.leaves())
       .join('g')
       .attr('transform', (item) => `translate (${item.x0},${item.y0})`);
+
     console.log(treeMapRoot.leaves());
-    const fader = (color) => d3.interpolateRgb(color, '#fff')(0.3);
+
+    const fader = (color) => d3.interpolateRgb(color, '#fff')(0.2);
     const colorScale = d3.scaleOrdinal(d3.schemeCategory10.map(fader));
+
+    const numberRound = (num) => {
+      return num.toString().replace(/(?=\d)(?=(\d{3})+(?!\d))/g, ' ');
+    };
+    const tooltip = d3
+      .select('body')
+      .append('div')
+      .attr('id', 'tooltip')
+      .style('visibility', 'hidden');
 
     nodes
       .append('rect')
@@ -46,16 +60,71 @@ export default function Treemap({ width, height }) {
       .attr('data-category', (item) => item.data.category)
       .attr('data-value', (item) => item.data.value)
       .attr('width', (item) => item.x1 - item.x0)
-      .attr('height', (item) => item.x1 - item.x0)
-      .attr('fill', (item) => colorScale(item.data.category));
+      .attr('height', (item) => item.y1 - item.y0)
+      .attr('fill', (item) => colorScale(item.data.category))
+      .on('mousemove', (event, item) => {
+        const [x, y] = pointer(event);
+
+        tooltip
+          .transition()
+          .duration(200)
+          .style('visibility', 'visible')
+          .attr('data-value', item.data.value);
+
+        tooltip
+          .style('left', x + 'px')
+          .style('top', y + 'px')
+          .style('position', 'absolute')
+
+          .style('display', 'inline-block')
+          .html(
+            `Name: ${item.data.name} <br /> Cost: ${numberRound(
+              Math.round(item.data.value)
+            )}$ <br /> Category: ${item.data.category}`
+          );
+      })
+      .on('mouseout', () => {
+        tooltip.transition().duration(200).style('visibility', 'hidden');
+      });
 
     const innerText = nodes
       .append('text')
       .attr('class', 'text')
       .attr('x', 3)
       .attr('y', 12)
-      .text((item) => `${item.data.name} `)
+      .text((item) => `${item.data.name}`)
       .attr('font-size', '0.7em');
+
+    const legendContainer = d3.select(legendRef.current);
+
+    legendContainer.selectAll('g').remove();
+
+    let categories = root.leaves().map((item) => item.data.category);
+
+    categories = categories.filter(
+      (category, index, self) => self.indexOf(category) === index
+    );
+    legendContainer.attr('width', width).attr('height', height);
+
+    const legend = legendContainer.selectAll('g').data(categories).join('g');
+
+    legend
+      .append('rect')
+      .attr('class', 'legend-item')
+      .attr('width', 12)
+      .attr('height', 12)
+      .attr('x', 12)
+      .attr('y', (item, i) => 12 * 2 * i)
+      .attr('fill', (item) => colorScale(item));
+
+    legend
+      .append('text')
+      .attr('id', 'legend')
+      .attr('transform', `translate(0, ${12})`)
+      .attr('x', 12 * 3)
+      .attr('y', (_, i) => 12 * 2 * i)
+      .style('font-size', 12)
+      .text((item) => item);
     return { treeMapRoot, innerText };
   };
 
@@ -79,10 +148,12 @@ export default function Treemap({ width, height }) {
       <h1 id='title'>Visualize Data with a Treemap Diagram</h1>
       <div id='description'>
         <p>KickStarterDledges</p>
-        <p>MoviesSales</p>
-        <p>VideoGamesSales</p>
+        {/* <p>MoviesSales</p>
+        <p>VideoGamesSales</p> */}
       </div>
       <svg ref={svgRef}></svg>
+      <h2>Legend by category</h2>
+      <svg ref={legendRef}></svg>
     </div>
   );
 }
